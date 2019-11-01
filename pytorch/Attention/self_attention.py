2226aa971febcf74d591b71b9d948fd6bb5d8ff0
gaurav <gauravgola96@gmail.com>
2019-11-01 23:48:49 +0530

    atten

--- pytorch/Attention/self_attention.py
+++ pytorch/Attention/self_attention.py
@@ -4,10 +4,20 @@ class AttentionHead(nn.Module):
     
     def __init__(self, d_model, d_feature, dropout=0.1):
         super().__init__()
+        #we will assume queries,key abd values all have same feature size
+        self.attn = ScaledDotProductAttention(dropout)
+        self.query = nn.Linear(d_model, d_feature)
+        self.key_tfm = nn.Linear(d_model, d_feature)
+        self.value_tfm = nn.Linear(d_model, d_feature)
+    
+    def forward(self, queries, keys, values, mask=None):
+        Q = self.query_tfm(queries) # (Batch, Seq, Feature)
+        K = self.key_tfm(keys) # (Batch, Seq, Feature)
+        V = self.value_tfm(values) # (Batch, Seq, Feature)
+        # compute multiple attention weighted sums
+        x = self.attn(Q, K, V)
+        return x
         
-
-
-
-
+        
 
 
