81df8034611516a7641e8e9f32b6e2fd1a93292a
gaurav <gauravgola96@gmail.com>
2019-11-02 01:07:44 +0530

    :heavy_plus_sign:

--- pytorch/Attention/self_attention.py
+++ pytorch/Attention/self_attention.py
@@ -1,12 +1,24 @@
 import torch.nn as nn
 
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout=0.01):
+        super().__init__()
+        
+            
+
+
+
+
+
+
 class AttentionHead(nn.Module):
     
     def __init__(self, d_model, d_feature, dropout=0.1):
         super().__init__()
         #we will assume queries,key abd values all have same feature size
         self.attn = ScaledDotProductAttention(dropout)
-        self.query = nn.Linear(d_model, d_feature)
+        self.query_tfm = nn.Linear(d_model, d_feature)
         self.key_tfm = nn.Linear(d_model, d_feature)
         self.value_tfm = nn.Linear(d_model, d_feature)
     
